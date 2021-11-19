0d5b697bee4b05cf1f6b79a19e7a0f869858d0e1
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-11-19 18:20:28 +0100

    trying different listening config 5

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,9 @@ async function bootstrap() {
   });
 
   const configService: ConfigService = app.get(ConfigService);
-  //await app.listen(configService.get('PORT') || 3004);  //  from best-playz
-  await app.listen(process.env.PORT || 8080, function(){
+  await app.listen(configService.get('PORT') || 8080);  //  from best-playz
+ /* await app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port); 
-  });   // https://shivamv12.medium.com/deploy-nestjs-on-heroku-in-5-simple-steps-cc7625ea6167
+  }); */  // https://shivamv12.medium.com/deploy-nestjs-on-heroku-in-5-simple-steps-cc7625ea6167
 }
 bootstrap();
